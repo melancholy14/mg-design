e4cd8f67b0ac7610520abab6407a836e6e8db2fd
Misol Goh <melancholy8914@gmail.com>
2020-07-12 14:04:57 +0200

    add storybook-addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,35 @@
+const path = require('path');
+
 module.exports = {
-  stories: ['../src/**/*.stories.tsx'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  stories: ['../src/**/*.stories.(tsx|mdx)'],
+  addons: [
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-a11y/register',
+    '@storybook/addon-knobs/register',
+    {
+      name: '@storybook/addon-storysource',
+      options: {
+        rule: {
+          include: [path.resolve(__dirname, '../src')],
+        },
+        loaderOptions: {
+          prettierConfig: {
+            printWidth: 80,
+            singleQuote: true,
+          }
+        }
+      }
+    },
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        configureJSX: true,
+        babelOptions: {},
+        sourceLoaderOptions: null,
+      }
+    }
+  ],
   webpackFinal: async config => {
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
@@ -12,6 +41,15 @@ module.exports = {
           loader: require.resolve('react-docgen-typescript-loader'),
         }
       ],
+    }, {
+      test: /\.stories\.tsx?$/,
+      loaders: [
+        {
+          loader: require.resolve('@storybook/source-loader'),
+          options: { parser: 'typescript' }
+        }
+      ],
+      enforce: 'pre',
     });
     config.resolve.extensions.push('.ts', '.tsx');
     return config;
